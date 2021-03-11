10ec03dce99d87e17c5216874a8835c806e81e53
jacobRinehart42 <jacobofmirkwood@yahoo.com>
2021-03-11 10:36:36 -0600

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,24 +8,22 @@ import { Component, VERSION } from "@angular/core";
 export class AppComponent {
   name = "Angular " + VERSION.major;
   showParagraph: boolean = false;
-  strings = [];
-  count = 0;
+  log = [];
 
   OnClick(): void {
     this.showParagraph = !this.showParagraph;
-    this.strings[this.count] = this.count + 1;
-    this.count++;
+    this.log[this.log.length] = this.log.length + 1;
   }
 
   getBColor(i) {
-    if (i >= 5) {
+    if (i >= 4) {
       return "green";
     }
     return "transparent";
   }
 
   whiteText(i) {
-    if (i >= 5) {
+    if (i >= 4) {
       return true;
     }
     return false;
